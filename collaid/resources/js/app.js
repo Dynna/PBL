7b31c5c7f24964542c6e00a0d37160606a2a98c4
marina01p <56072634+marina01p@users.noreply.github.com>
2020-11-26 04:50:34 +0200

    refactor

--- collaid/resources/js/app.js
+++ collaid/resources/js/app.js
@@ -1,1 +1,19 @@
 require('./bootstrap');
+
+function dropdownFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.login-btn')) {
+        var dropdowns = document.getElementsByClassName("login-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
