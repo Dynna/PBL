fcedf534d19618a05c9a302b1421c96c8bd3f440
oliviasv <62649380+oliviasv@users.noreply.github.com>
2020-12-12 03:17:50 +0200

    post-page refactor

--- collaid/resources/views/posts/index.blade.php
+++ collaid/resources/views/posts/index.blade.php
@@ -47,6 +47,30 @@
         .posts-list {
         }
 
+        .post {
+            border-bottom: 1px #ccc solid;
+            height: 130px;
+        }
+
+        .edit-btn {
+            width: 110px;
+            height: 35px;
+            background: #aabcbf;
+            text-align: center;
+            border: none;
+            color: white;
+            outline: none;
+        }
+
+        .delete-btn {
+            width: 110px;
+            height: 35px;
+            background: white;
+            border: 1px #ccc solid;
+            color: #aabcbf;
+            outline: none;
+        }
+
     </style>
 
     <div class="container-main">
@@ -54,22 +78,25 @@
         <div class="posts-handing">
         @can('create', \App\Post::class)
             <h1>POSTS</h1>
-            <a href="{{ route('posts.create') }}" class="new-post-btn">Create post</a>
+            <a href="{{ route('posts.create') }}" class="new-post-btn">New Post</a>
         @endcan
         </div>
         <div class="posts-list">
         @foreach($posts as $post)
+            <div class="post">
             <h3>{{ $post->post_title }}</h3>
             <p>{{ $post->post_content}}</p>
-            <p>
+
               {{--  <a href="{{ route('posts.show', $post->id) }}" class="btn btn-info">View post</a>--}}
                 @can('update', $post)
-                <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-primary">Edit post</a>
+                <a href="{{ route('posts.edit', $post->id) }}">
+                    <button type="button" class="edit-btn">Edit</button>
+                </a>
                 <a href="{{ route('post.delete', $post->id) }}">
-                        <button type="button" class="btn btn-danger btn-sm">Delete Post</button>
+                        <button type="button" class="delete-btn">Delete</button>
                 </a>
                 @endcan
-            </p>
+            </div>
 
         @endforeach
         </div>
